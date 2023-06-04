751900bd52e31aa764ba5546f4af461bf0b8d287
akshat170902 <akshat.ug20@nsut.ac.in>
2023-06-04 13:12:37 +0530

    week 4

--- week1 day4/index.js
+++ week1 day4/index.js
@@ -33,3 +33,48 @@ function sum2(num,value,...arg){
 let ans=sum2(1,2,3,4,5,6,7);
 console.log(ans); 
 
+function interest(p,r=6,t=1){
+    return p*r*t/100;
+}
+console.log(interest(1000,undefined,8));
+
+let person={
+    fName:'Akshat',
+    Lname:'Garg',
+    get fullName(){
+        return `${person.fName} ${person.Lname}`
+    },
+    set fullName(value){
+        console.log(typeof(value));
+    
+
+        if(typeof(value) !== 'string'){
+            console.log(typeof(value));
+            throw new Error("you have not sent a string ");
+        }
+        let parts=value.split(' ') ;
+        this.fName=parts[0];
+        this.Lname=parts[1];
+    }
+}
+try{
+    person.fullName='true';
+}
+catch(e){
+    alert(e);
+}
+
+console.log(person.fullName);
+{
+    var a=5;
+}
+console.log(a);
+{
+    var a=6;
+}
+console.log(a);
+
+// Reduce 
+let arr=[1,2,3,4];
+let totalSum=arr.reduce((accumulator,currentValue)=>accumulator+currentValue,0);
+console.log(totalSum);
